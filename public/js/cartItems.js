2d1b3841483de6aec1596832661ac13bb179e192
teojincheng <jinchengproject@gmail.com>
2017-06-05 10:35:00 +0800

    fade table row

--- public/js/cartItems.js
+++ public/js/cartItems.js
@@ -1,14 +1,16 @@
 /* 
  *Author: Teo Jin Cheng
+ *
+ *When user clicks on the remove item link on the shopping cart page, 
+ *make the table row of that toy disappear to simulate 
+ *removing from cart.  
  */
-
-
 
 
 /**
  * 
- * @param {type} num
- * @returns {undefined}
+ * @param {type} num unique id of the tr we want to make disappear
+ * 
  */
 
 function fadeFunction(num) {
@@ -18,33 +20,27 @@ function fadeFunction(num) {
     $("#tr" + o).fadeOut("slow");
 }
 
+/**
+ * 
+ * @param {type} num unique id of the link element that the user clicks to 
+ * reomove cart iem. 
+ * 
+ */
+
+function addListener(num) {
+    var n = "del" + num.toString();
+    document.getElementById(n).addEventListener("click", function () {
+        fadeFunction(num);
+    });
+}
 
 
 var numToLoop = counter;
 
-
-function woah(numo){
-    alert(numo);
-}
-
-function doThis(num){
-    var n = "del" + num.toString();
-    document.getElementById(n).addEventListener("click",function(){woah(num);});
-}
-
-
-for(i = 0; i < numToLoop; i++){
-    doThis(i);
-}
-/*
+/* 
+ * For every remove item link the shopping cart register the click listener. 
+ * */
 for (i = 0; i < numToLoop; i++) {
-    (function () {
-
-        var n = "del" + i.toString();
-        document.getElementById(n).addEventListener("click", function () {
-            fadeFunction(i);
-        });
-    }());
+    addListener(i);
 }
 
-*/
